06c8338563765b81381bd02b5846d1c333886c0d
abhi4334 <abhi_4334@yahoo.co.in>
2014-04-27 12:13:32 -0700

    Revert "First Commit"
    This reverts commit f6ce0101212776dd490649f38b72f419e78d508c.

--- targeted-advertisements/src/main/java/org/springframework/social/targeted/adv/account/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.social.targeted.adv.account;
-
-public interface AccountRepository {
-	
-	void createAccount(Account account) throws UsernameAlreadyInUseException;
-
-	Account findAccountByUsername(String username);
-	
-}
